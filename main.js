49a9c2768920e4baf662e56cec4806eccc41300b
Adão Caculo Agostinho <44007428+adcmadacarlos70@users.noreply.github.com>
2018-11-18 20:04:29 +0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$("ul").hide();
-	$("ul li:odd").css("background-color", "#efefef");
-	$("h3 span").click(function(){
-		$(this).parent().next().slideToggle();
-	});
-});
